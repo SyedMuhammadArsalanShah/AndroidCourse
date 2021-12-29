a0b0ca9af608fc98ba1a634179100af70d633f89
MaryamHafeezKhan <smarsalanshah125@gmail.com>
2021-12-29 22:47:12 +0500

    lecture is updated

---
+++ Lecture13ProgressBarAndSeekBar/app/src/main/java/com/example/lecture13progressbarandseekbar/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.lecture13progressbarandseekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ProgressBar p1, p2;
+    TextView textView;
+    Button button;
+    int i = 0;
+    Handler handler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        p1 = findViewById(R.id.progressBar);
+        p2 = findViewById(R.id.progressBar2);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i = p2.getProgress();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (i < 100) {
+                            i += 1;
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    p2.setProgress(i);
+                                    textView.setText(i + "/" + p2.getMax());
+                                     handler.postDelayed(new Runnable() {
+                                         @Override
+                                         public void run() {
+
+                                             Intent intent =new Intent(MainActivity.this, SeekBarAndItsType.class);
+                                             startActivity(intent);
+                                         }
+                                     }, 3000 );
+
+                                }
+                            });
+                            try {
+                                Thread.sleep(100);
+
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+
+                        }
+
+
+                    }
+                }).start();
+
+
+            }
+        });
+
+
+    }
+}
---
+++ Lecture13ProgressBarAndSeekBar/app/src/main/java/com/example/lecture13progressbarandseekbar/SeekBarAndItsType.java
@@ -0,0 +1,47 @@
+package com.example.lecture13progressbarandseekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SeekBarAndItsType extends AppCompatActivity {
+SeekBar s1,s2;
+TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seek_bar_and_its_type);
+        s1 =findViewById(R.id.seekBar);
+        s2 =findViewById(R.id.seekBar2);
+        textView=findViewById(R.id.textView2);
+        s1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+
+            int seek=0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+             seek = i  ;
+            textView.setText(String.valueOf(seek));
+           //  Toast.makeText(SeekBarAndItsType.this,"this is our seekbar value"+seek, Toast.LENGTH_SHORT).show();
+
+            };
+
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+               // textView.setText(String.valueOf(seek));
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+               // textView.setText(String.valueOf(seek));
+            }
+        });
+
+    }
+}
