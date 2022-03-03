596f92d2a36fe086bc6b3646307359350b6e1309
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-03-03 20:18:19 +0500

    code updated

---
+++ Lecture40BasicActivity/app/src/main/java/com/example/lecture40basicactivity/FirstFragment.java
@@ -0,0 +1,50 @@
+package com.example.lecture40basicactivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.lecture40basicactivity.databinding.FragmentFirstBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "next button clicked", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+//                NavHostFragment.findNavController(FirstFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
